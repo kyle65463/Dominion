8c98b2de11985ad662c16e6d01be19bc45d5d116
Leo Hsieh <smile04455660@gmail.com>
2021-07-01 01:54:14 +0800

    new file

---
+++ src/dominion/models/cards/actions/Workshop.java
@@ -0,0 +1,7 @@
+package dominion.models.cards.actions;
+
+import dominion.models.cards.Card;
+import dominion.models.events.game.HasDisplayedCardsSelection;
+
+public class Workshop extends Card implements Action, HasDisplayedCardsSelection {
+}
